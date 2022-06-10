0c0c79277ca3bf69cd8e9ec214da1a44ee6ccbe1
Selena Nguyen <missselenah@gmail.com>
2022-06-10 14:23:38 -0700

    requestmapping

--- src/main/java/com/selenanguyen/daikichiroutes/controllers/DaikichiController.java
+++ src/main/java/com/selenanguyen/daikichiroutes/controllers/DaikichiController.java
@@ -1,6 +1,7 @@
 package com.selenanguyen.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,21 @@ public class DaikichiController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be oopen to new ideas!";
 	}
+	
+	@RequestMapping("/travel/{pathLocation}")
+	public String path(@PathVariable("pathLocation")String name) {
+		return "Congratulations! You will soon travel to " + name;
+	}
+	
+	@RequestMapping("/lotto/{pathNumber}")
+	public String path(@PathVariable("pathNumber")Integer number) {
+		if(number % 2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+		
+	}
+	
 
 }
